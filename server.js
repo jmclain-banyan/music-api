6260fc7a36a20ba22279a6dc7974a32592a803da
Josh McLain <joshmclain45@gmail.com>
2021-05-14 23:09:57 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
